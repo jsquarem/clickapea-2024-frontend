778a8a9f7db271267f58b73b3da2ee881f3ead26
jmartymar <jeffjmart@gmail.com>
2024-06-12 00:53:23 -0500

    add new hopepage content and styles

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,104 +1,176 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import AddRecipeForm from '../../components/AddRecipeForm/AddRecipeForm';
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  Move,
+  batch,
+} from 'react-scroll-motion';
+import './HomePage.css';
 
 const HomePage = () => {
+  const colors = [
+    '#FFFFFF',
+    '#FF6347', // Tomato for 'Recipe Management'
+    '#FFD700', // Gold for 'Ingredient Conversion'
+    '#ADFF2F', // GreenYellow for 'Interactive Instructions'
+    '#20B2AA', // LightSeaGreen for 'Shopping List'
+    '#F5F5DC', // Beige for 'Save Recipes'
+    '#E0FFFF', // LightCyan for 'Nutritional Information'
+  ];
+
+  const [scrollProgress, setScrollProgress] = useState(0);
+  const formRef = useRef(null);
+  const initialFormTop = useRef(null); // To store the initial top position of the form
+  const [formSticky, setFormSticky] = useState(false);
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+  const sectionsRefs = useRef([]);
+  const [currentSection, setCurrentSection] = useState(0);
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= 768);
+  };
+
+  const handleScroll = () => {
+    const scrollTop = window.scrollY;
+    const scrollHeight =
+      document.documentElement.scrollHeight - window.innerHeight;
+    setScrollProgress(scrollTop / scrollHeight);
+
+    if (initialFormTop.current === null) {
+      initialFormTop.current = formRef.current.offsetTop;
+    }
+
+    const formTop = formRef.current.getBoundingClientRect().top;
+
+    if (formTop <= 150) {
+      setFormSticky(true);
+    } else if (scrollTop <= initialFormTop.current - 150) {
+      setFormSticky(false);
+    }
+
+    if (scrollTop === 0) {
+      // Reset to initial state when scrolled to the top
+      setFormSticky(false);
+    }
+
+    // Update current section based on scroll position
+    const currentIndex = sectionsRefs.current.findIndex(
+      (section) =>
+        section.current.getBoundingClientRect().top >= 0 &&
+        section.current.getBoundingClientRect().top < window.innerHeight / 2
+    );
+    setCurrentSection(
+      currentIndex >= 0 ? currentIndex : sectionsRefs.current.length - 1
+    );
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [currentSection]);
+
   return (
-    <div>
-      <header className="text-center py-8 bg-white">
-        <h1 className="text-3xl lg:text-4xl font-bold">
+    <div className="relative">
+      <div className="text-center py-8 bg-white">
+        <h1 className="text-4xl lg:text-6xl font-bold text-orange-800">
           Welcome to Clickapea!
         </h1>
-        <p className="text-xl text-gray-600">Your ultimate sous chef bestie</p>
-      </header>
-
-      <section className="py-8 bg-white max-w-6xl mx-auto">
-        <div className="w-full flex justify-center pb-16">
-          <div className="w-11/12 lg:w-1/2 text-center bg-gray-100 border border-gray-300 p-6 rounded-lg shadow-md">
+        <p className="text-xl text-orange-600 pb-10 lg:pb-0">
+          Your ultimate sous chef bestie
+        </p>
+        <div
+          className={`${formSticky ? 'form-sticky z-10' : 'initial-form-position bg-white p-6'} transition-all flex justify-center`}
+          ref={formRef}
+        >
+          <div className="w-11/12 lg:w-2/3 mt-[-30px] lg:mt-0">
             <AddRecipeForm />
           </div>
         </div>
-        <h2 className="text-3xl font-semibold text-center mb-8">
-          Key Features
-        </h2>
-        <div className="mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          <div className="text-center">
-            <h3 className="text-2xl font-semibold mb-2">Recipe Management</h3>
-            <p className="text-gray-600 px-2">
-              Organize and store all your favorite recipes in one place. Our
-              easy-to-use interface allows you to quickly add, edit, and search
-              for recipes.
-            </p>
-          </div>
-          <div className="text-center">
-            <h3 className="text-2xl font-semibold mb-2">
-              Ingredient Conversion
-            </h3>
-            <p className="text-gray-600 px-2">
-              Effortlessly convert ingredients between metric and imperial
-              units. Clickapea takes the guesswork out of measurements.
-            </p>
-          </div>
-          <div className="text-center">
-            <h3 className="text-2xl font-semibold mb-2">
-              Interactive Instructions
-            </h3>
-            <p className="text-gray-600 px-2">
-              Follow step-by-step cooking instructions with ease. Our
-              interactive guide ensures you never miss a step.
-            </p>
-          </div>
-          <div className="text-center">
-            <h3 className="text-2xl font-semibold mb-2">Shopping List</h3>
-            <p className="text-gray-600 px-2">
-              Create and manage your shopping list directly from your recipes.
-              Clickapea helps you stay organized and prepared.
-            </p>
-          </div>
-          <div className="text-center">
-            <h3 className="text-2xl font-semibold mb-2">Save Recipes</h3>
-            <p className="text-gray-600 px-2">
-              Easily save recipes from all over the internet with just a web
-              address. Clickapea fetches and processes all the details for you,
-              making it simple to keep all your favorite recipes in one place.
-            </p>
-          </div>
-          <div className="text-center">
-            <h3 className="text-2xl font-semibold mb-2">
-              Nutritional Information
-            </h3>
-            <p className="text-gray-600 px-2">
-              Get detailed nutritional information for all your recipes.
-              Clickapea helps you make informed decisions about your meals.
-            </p>
+        <div className="text-center text-2xl flex flex-col gap-4 w-full px-10 lg:px-30 tracking-tight">
+          <div>
+            Welcome to Clickapea, your ultimate sous chef bestie! Discover a
+            world of culinary delights with our user-friendly platform. Whether
+            you're a seasoned chef or just starting, Clickapea helps you manage
+            recipes, convert ingredients, follow step-by-step instructions, and
+            create organized shopping lists, digitize your collection! Join us
+            and make your cooking experience seamless and enjoyable.
           </div>
         </div>
-      </section>
+      </div>
 
-      <section className="py-12 bg-gray-50">
-        <h2 className="text-3xl font-semibold text-center mb-8">Happy Users</h2>
-        <div className="max-w-4xl mx-auto space-y-8">
-          <blockquote className="text-center text-gray-600 italic">
-            <p>
-              “Clickapea has revolutionized my cooking experience. It's like
-              having a personal sous chef!”
-            </p>
-            <footer className="mt-2">- Alex</footer>
-          </blockquote>
-          <blockquote className="text-center text-gray-600 italic">
-            <p>
-              “The ingredient conversion feature is a game-changer for me. No
-              more struggling with measurements!”
-            </p>
-            <footer className="mt-2">- Jamie</footer>
-          </blockquote>
-        </div>
-      </section>
+      <ScrollContainer>
+        {features.map((feature, index) => (
+          <ScrollPage key={index}>
+            <div
+              style={{
+                height: isMobile ? '80vh' : '80vh',
+                position: 'relative',
+                zIndex: 1,
+                marginTop: isMobile ? -110 : -200,
+              }}
+              className="flex flex-col w-full justify-center items-center px-2 lg:px-10"
+            >
+              <Animator animation={batch(Move(0, 0, 0, 0))}>
+                <h3 className="text-4xl lg:text-6xl font-semibold mb-4 text-orange-800">
+                  {feature.title}
+                </h3>
+              </Animator>
+              <div className="flex flex-row justify-center items-center">
+                <div className="w-5/12 text-right">
+                  <Animator
+                    animation={
+                      isMobile
+                        ? batch(Move(-100, 0, -100, 100))
+                        : batch(Move(-1000, 1000, -1000, -500))
+                    }
+                  >
+                    <img
+                      src={feature.image}
+                      alt={feature.title}
+                      className="object-cover w-full h-full"
+                    />
+                  </Animator>
+                </div>
 
-      <footer className="py-12 bg-gray-900 text-white text-center">
+                <div className="feature-content w-7/12 pl-2 text-left text-3xl lg:text-4xl tracking-tight">
+                  <Animator
+                    animation={
+                      isMobile
+                        ? batch(Move(30, 100, 30, 0))
+                        : batch(Move(1000, 0, 1000, 0))
+                    }
+                  >
+                    <p className="text-gray-600">{feature.description}</p>
+                  </Animator>
+                </div>
+              </div>
+            </div>
+            <div
+              className="absolute top-0 left-0 w-full h-full"
+              style={{
+                background: `linear-gradient(to bottom, ${colors[index]}, ${
+                  colors[index + 1] || colors[index]
+                })`,
+                opacity: scrollProgress,
+                transition: 'opacity 0.5s',
+                zIndex: 0,
+              }}
+            ></div>
+          </ScrollPage>
+        ))}
+      </ScrollContainer>
+
+      <footer className="py-12 bg-orange-800 text-white text-center">
         <Link
           to="/signup"
-          className="inline-block bg-orange-500 text-white text-lg font-semibold py-3 px-6 rounded hover:bg-orange-600 transition"
+          className="inline-block bg-yellow-500 text-white text-lg font-semibold py-3 px-6 rounded hover:bg-yellow-600 transition"
         >
           Join Clickapea Today!
         </Link>
@@ -106,5 +178,44 @@ const HomePage = () => {
     </div>
   );
 };
+
+const features = [
+  {
+    title: 'Recipe Management',
+    description:
+      'Organize and store all your favorite recipes in one place. Our easy-to-use interface allows you to quickly add, edit, and search for recipes.',
+    image: '/assets/images/homepage/manage.png',
+  },
+  {
+    title: 'Ingredient Conversion',
+    description:
+      'Effortlessly convert ingredients between metric and imperial units. Clickapea takes the guesswork out of measurements.',
+    image: '/assets/images/homepage/convert.png',
+  },
+  {
+    title: 'Interactive Instructions',
+    description:
+      'Follow step-by-step cooking instructions with ease. Our interactive guide ensures you never miss a step.',
+    image: '/assets/images/homepage/interact.png',
+  },
+  {
+    title: 'Shopping List',
+    description:
+      'Create and manage your shopping list directly from your recipes. Clickapea helps you stay organized and prepared.',
+    image: '/assets/images/homepage/shop.png',
+  },
+  {
+    title: 'Save Recipes',
+    description:
+      'Easily save recipes from all over the internet with just a web address. Clickapea fetches and processes all the details for you, making it simple to keep all your favorite recipes in one place.',
+    image: '/assets/images/homepage/save.png',
+  },
+  {
+    title: 'Nutritional Information',
+    description:
+      'Get detailed nutritional information for all your recipes. Clickapea helps you make informed decisions about your meals.',
+    image: '/assets/images/homepage/nutrition.png',
+  },
+];
 
 export default HomePage;
